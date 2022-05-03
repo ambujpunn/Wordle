498b00698ccf7e4d190a5982de4ba931da3863e9
Ambuj Punn <ambujpunn@gmail.com>
2022-05-03 17:14:44 -0400

    Initial Commit

---
+++ Wordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wordle
+//
+//  Created by Ambuj Punn on 5/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Ambuj Punn on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
